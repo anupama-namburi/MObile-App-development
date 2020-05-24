4a38a4d65c70cea2005b443a590f498cedc06144
anupama-namburi <namburianupama@gmail.com>
2020-05-25 00:38:34 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
 
   EditText sid,sname,suniversity;
+  TextView tv;
     ContentValues cv;
     DatabaseReference myRef;
     SaveData i;
+    String userenterid;
   Button save, read;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         suniversity=findViewById(R.id.editText4);
         save=findViewById(R.id.button);
         read=findViewById(R.id.button2);
+        tv=findViewById(R.id.textView);
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
          myRef = database.getReference("Student");
@@ -43,19 +47,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
             SaveData sd=new SaveData(sid.getText().toString(),sname.getText().toString(),suniversity.getText().toString());
-            myRef.child("sid").setValue(sd);
+            myRef.child(sid.getText().toString()).setValue(sd);
             }
         });
 
         read.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Query result=myRef.orderByChild("sid");
+                userenterid=sid.getText().toString();
+            Query result=myRef.orderByChild("sid").equalTo(userenterid);
             result.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    String uname=dataSnapshot.child("sid").child("sname").getValue(String.class);
-                    Toast.makeText(getApplicationContext(),uname.toString(),Toast.LENGTH_LONG).show();
+                    String uname=dataSnapshot.child(userenterid).child("sid").getValue(String.class);
+                    String id=dataSnapshot.child(userenterid).child("sname").getValue(String.class);
+                    String uni=dataSnapshot.child(userenterid).child("suniversity").getValue(String.class);
+                    StringBuilder sb=new StringBuilder();
+                    sb.append("student id:"+id+"\n");
+                    sb.append("student Name:"+uname+"\n");
+                    sb.append("University:"+uni+"\n");
+                    tv.setText(sb);
+
                 }
 
                 @Override
